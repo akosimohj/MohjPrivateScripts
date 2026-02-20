d479743c262b58ed65951d899a556c8e94280c41
akosimohj <93630827+akosimohj@users.noreply.github.com>
2026-02-21 03:24:46 +0800

    Update MohjScripts.lua

--- MohjScripts.lua
+++ MohjScripts.lua
@@ -1,81 +1,15 @@
--- [[ SOLO HUNTERS PRIVATE SCRIPT ]]
--- Wait for game to load fully
-repeat task.wait() until game:IsLoaded()
+print("--- STARTING JUMP TEST ---")
 
--- Load Orion Library (Stable GitHub Link)
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com')))()
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
 
--- Create Main Window
-local Window = OrionLib:MakeWindow({
-    Name = "Solo Hunters | Private Suite", 
-    HidePremium = true, 
-    SaveConfig = true, 
-    ConfigFolder = "SoloHunters_Private"
-})
-
--- tabs
-local MainTab = Window:MakeTab({
-	Name = "Main Features",
-	Icon = "rbxassetid://4483362458",
-	PremiumOnly = false
-})
-
--- [[ AUTO LOOT TOGGLE ]]
-MainTab:AddToggle({
-	Name = "Auto-Loot Gems/Gold",
-	Default = false,
-	Callback = function(Value)
-		_G.AutoLoot = Value
-		task.spawn(function()
-			while _G.AutoLoot do
-				-- Scan Workspace for dropped items
-				for _, v in pairs(game.Workspace:GetChildren()) do
-					if v.Name == "Gem" or v.Name == "Gold" or v.Name == "Coin" then
-						if v:IsA("BasePart") then
-							v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-                            v.CanCollide = false
-						end
-					end
-				end
-				task.wait(0.3)
-			end
-		end)
-	end    
-})
-
--- [[ SPEED SLIDER ]]
-MainTab:AddSlider({
-	Name = "Walkspeed",
-	Min = 16,
-	Max = 250,
-	Default = 16,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "Speed",
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-	end    
-})
-
--- [[ JUMP POWER ]]
-MainTab:AddSlider({
-	Name = "Jump Power",
-	Min = 50,
-	Max = 300,
-	Default = 50,
-	Increment = 1,
-	ValueName = "Power",
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-	end    
-})
-
-OrionLib:Init()
-
--- Notification
-OrionLib:MakeNotification({
-	Name = "Script Loaded!",
-	Content = "Welcome to your private Solo Hunters script.",
-	Image = "rbxassetid://4483362458",
-	Time = 5
-})
+-- This will make you jump 5 times automatically to prove the script is running
+task.spawn(function()
+    for i = 1, 5 do
+        print("Jumping... " .. i)
+        humanoid.Jump = true
+        task.wait(1)
+    end
+    print("--- TEST COMPLETE ---")
+end)
